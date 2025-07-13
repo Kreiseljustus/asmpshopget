6d8e15302a68a1f4ac4b5d51ed9b7908096e7eea
Justus Walter-Helk <justus.walter-helk@web.de>
2025-07-13 15:45:47 +0200

    Update to 1.21.7

--- src/main/java/io/github/kreiseljustus/asmpshopget/Asmpshopget.java
+++ src/main/java/io/github/kreiseljustus/asmpshopget/Asmpshopget.java
@@ -30,7 +30,7 @@ import java.util.regex.Pattern;
 
 public class Asmpshopget implements ModInitializer {
 
-    static final String VERSION = "1.0.15";
+    static final String VERSION = "1.0.16";
     static final String VERSION_URL = "https://kreiseljustus.com/asmp_version.txt";
 
     public static ModConfig s_Config;
