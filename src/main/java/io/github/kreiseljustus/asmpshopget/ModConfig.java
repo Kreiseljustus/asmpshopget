dc4ecdc9abe5c9fe1a5967dee096d59ef30aa3a8
Justus Walter-Helk <justus.walter-helk@web.de>
2025-07-16 15:53:42 +0200

    added config categories

--- src/main/java/io/github/kreiseljustus/asmpshopget/ModConfig.java
+++ src/main/java/io/github/kreiseljustus/asmpshopget/ModConfig.java
@@ -3,17 +3,25 @@ package io.github.kreiseljustus.asmpshopget;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 @Config(name = "asmpshopget")
 public class ModConfig implements ConfigData {
+    @ConfigEntry.Category("General")
     public boolean enable = true;
+    @ConfigEntry.Category("General")
     public boolean trackShops = true;
+    @ConfigEntry.Category("General")
     public boolean trackWaystones = true;
+    @ConfigEntry.Category("General")
     public String postUrl = "https://kreiseljustus.com/asmp/post";
+    @ConfigEntry.Category("General")
     public int ticksBetweenSends = 600;
 
+    @ConfigEntry.Category("Dev")
     public boolean enableDebugMode = false;
+    @ConfigEntry.Category("Dev")
     public boolean allowOnAllServers = false;
 
     public static void register() {
