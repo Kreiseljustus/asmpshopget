68f4880f2486fb9ce2a0d7f6acf12ef12833d5fa
Crisel <justus.walter-helk@web.de>
2025-10-15 00:36:30 +0200

    version bump for alpha

--- src/main/java/io/github/kreiseljustus/asmputils/Constants.java
+++ src/main/java/io/github/kreiseljustus/asmputils/Constants.java
@@ -1,6 +1,6 @@
 package io.github.kreiseljustus.asmputils;
 
 public class Constants {
-    static final String VERSION = "1.2.0";
+    static final String VERSION = "1.3.0";
     static final String VERSION_URL = "https://kreiseljustus.com/asmp_version.txt";
 }
