13dcb2bfd88b340ab98cd1f5e2a48e5f493e673f
Crisel <justus.walter-helk@web.de>
2026-01-10 20:53:41 +0100

    CORS fix

--- src/main/java/io/github/kreiseljustus/asmputils/Constants.java
+++ src/main/java/io/github/kreiseljustus/asmputils/Constants.java
@@ -1,6 +1,6 @@
 package io.github.kreiseljustus.asmputils;
 
 public class Constants {
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     public static final String VERSION_URL = "https://asmp.mia.jetzt/asmp_version.txt";
 }
