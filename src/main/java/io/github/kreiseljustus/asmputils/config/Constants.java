1371125974c9695c367545ba4fd8608a7e454f2e
Crisel <justus.walter-helk@web.de>
2026-04-17 16:01:49 +0200

    version bump

--- src/main/java/io/github/kreiseljustus/asmputils/config/Constants.java
+++ src/main/java/io/github/kreiseljustus/asmputils/config/Constants.java
@@ -1,7 +1,7 @@
 package io.github.kreiseljustus.asmputils.config;
 
 public class Constants {
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.4.2";
     @Deprecated
     public static final String VERSION_URL = "https://asmp.mia.jetzt/asmp_version.txt";
     public static final String PROJECT_ID = "9qOjeThL";
